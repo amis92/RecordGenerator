f319ea49a43c45aaee1c0d6a49368d54c59c3491
Amadeusz Sadowski <amis.tau.92@gmail.com>
2017-03-30 01:02:14 +0200

    RecordAttribute properties prepared for handling

--- src/Amadevus.RecordGenerator/Utilities/RecordAttributeExtensions.cs
+++ src/Amadevus.RecordGenerator/Utilities/RecordAttributeExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Amadevus.RecordGenerator
 {
@@ -24,6 +26,55 @@ namespace Amadevus.RecordGenerator
                 .DescendantNodesAndSelf()
                 .LastOrDefault(node => node is IdentifierNameSyntax) as IdentifierNameSyntax;
             return identifierNameSyntax?.Identifier.ValueText;
+        }
+
+        public static AttributeSyntax ExtractRecordAttribute(this TypeDeclarationSyntax typeDeclaration)
+        {
+            return typeDeclaration.AttributeLists
+                   .SelectMany(attListSyntax => attListSyntax.Attributes.Where(att => att.IsRecordAttributeSyntax()))
+                   .FirstOrDefault();
+        }
+
+        public static string GetPrimaryCtorAccess(this AttributeSyntax syntax)
+        {
+            var argument = syntax.ArgumentList.Arguments
+                .FirstOrDefault(
+                    x => x.NameEquals.Name.Identifier.ValueText == RecordAttributeProperties.PrimaryCtorAccessName);
+            var token = (argument?.Expression as LiteralExpressionSyntax)?.Token ?? default(SyntaxToken);
+            return token.IsKind(SyntaxKind.StringLiteralToken)
+                ? MapToCorrectAccessValueOrDefault(token.ValueText)
+                : RecordAttributeProperties.PrimaryCtorAccessDefault;
+        }
+
+        private static string MapToCorrectAccessValueOrDefault(string access)
+        {
+            return access == "public" || access == "protected" || access == "private"
+                ? access
+                : RecordAttributeProperties.PrimaryCtorAccessDefault;
+        }
+
+        public static bool GetGenerateMutators(this AttributeSyntax syntax)
+        {
+            var argument = syntax.ArgumentList.Arguments
+                .FirstOrDefault(
+                    x => x.NameEquals.Name.Identifier.ValueText == RecordAttributeProperties.GenerateMutatorsName);
+            var token = (argument?.Expression as LiteralExpressionSyntax)?.Token ?? default(SyntaxToken);
+            return token.IsKind(SyntaxKind.FalseKeyword)
+                || token.IsKind(SyntaxKind.TrueKeyword)
+                ? (bool)token.Value
+                : RecordAttributeProperties.GenerateMutatorsDefault;
+        }
+
+        public static bool GetGenerateCollectionMutators(this AttributeSyntax syntax)
+        {
+            var argument = syntax.ArgumentList.Arguments
+                .FirstOrDefault(
+                    x => x.NameEquals.Name.Identifier.ValueText == RecordAttributeProperties.GenerateCollectionMutatorsName);
+            var token = (argument?.Expression as LiteralExpressionSyntax)?.Token ?? default(SyntaxToken);
+            return token.IsKind(SyntaxKind.FalseKeyword)
+                || token.IsKind(SyntaxKind.TrueKeyword)
+                ? (bool)token.Value
+                : RecordAttributeProperties.GenerateCollectionMutatorsDefault;
         }
     }
 }
---
+++ src/Amadevus.RecordGenerator/Utilities/RecordAttributeProperties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amadevus.RecordGenerator
+{
+    internal class RecordAttributeProperties
+    {
+        public const string PrimaryCtorAccessDefault = "public";
+        public const string PrimaryCtorAccessName = nameof(RecordAttribute.PrimaryCtorAccess);
+        public const bool GenerateMutatorsDefault = true;
+        public const string GenerateMutatorsName = nameof(RecordAttribute.GenerateMutators);
+        public const bool GenerateCollectionMutatorsDefault = true;
+        public const string GenerateCollectionMutatorsName = nameof(RecordAttribute.GenerateCollectionMutators);
+    }
+}
