10b59b635374b1545e4f56281d333c84413025a3
Amadeusz Sadowski <amis.tau.92@gmail.com>
2018-12-05 17:10:22 +0100

    Add test that asserts records don't have RecordAttribute in reflection

---
+++ test/Amadevus.RecordGenerator.Test/RecordClassTests.cs
@@ -0,0 +1,18 @@
+﻿using Amadevus.RecordGenerator.TestsBase;
+using Xunit;
+
+namespace Amadevus.RecordGenerator.Test
+{
+    public class RecordClassTests : RecordTestsBase
+    {
+        [Fact]
+        public void ReflectedClass_HasNo_RecordAttribute()
+        {
+            var recordType = typeof(Item);
+
+            var recordAttributes = recordType.GetCustomAttributes(typeof(RecordAttribute), false);
+
+            Assert.Empty(recordAttributes);
+        }
+    }
+}
