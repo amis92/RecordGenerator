amis92/RecordGenerator

commit 8ac523cfd654538a700d8e4d987470746f127dcb

Fix naming of a with-member parameter

src/Amadevus.RecordGenerator.Generators/RecordPartialGenerator.cs
@@ -112,7 +112,7 @@ namespace Amadevus.RecordGenerator.Generators
                     .AddModifiers(SyntaxKind.PublicKeyword)
                     .WithParameters(
                         Parameter(
-                            entry.Identifier)
+                            Identifier(Names.Value))
                         .WithType(entry.Type))
                     .WithBodyStatements(
                         ReturnStatement(

src/Amadevus.RecordGenerator.Generators/Names.cs
@@ -1,5 +1,8 @@
 ﻿namespace Amadevus.RecordGenerator.Generators
 {
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
     internal static class Names
     {
         public const string WithPrefix = "With";
@@ -15,6 +18,8 @@
         public const string ToImmutable = "ToImmutable";
         public const string Validate = "Validate";
         public new const string ToString = nameof(object.ToString);
+
+        public const string Value = "value";
 
         public const string ToolName = "Amadevus.RecordGenerator";
         public const string GeneratedCodeAttribute = "System.CodeDom.Compiler.GeneratedCodeAttribute";

test/Amadevus.RecordGenerator.Test/RecordNamingConventionsTests.cs
@@ -1,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+using Amadevus.RecordGenerator.TestsBase;
+
+namespace Amadevus.RecordGenerator.Test
+{
+    public class GeneratedTypesNamingConventionsTests : RecordTestsBase
+    {
+        public static readonly IEnumerable<object[]> ParameterDataSource =
+            from t in new[]
+            {
+                typeof(Item)           , typeof(Item.Builder),
+                typeof(Container)      , typeof(Container.Builder),
+                typeof(GenericRecord<>), typeof(GenericRecord<>.Builder),
+            }
+            from m in t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public
+                                                             | BindingFlags.Instance
+                                                             | BindingFlags.Static)
+            from p in m.GetParameters()
+            select new[] { t.Name, m.Name, p.Name };
+
+        [Theory]
+        [MemberData(nameof(ParameterDataSource))]
+        public void Parameter_Name_Uses_Camel_Case(
+            #pragma warning disable xUnit1026
+            string type, string method,
+            #pragma warning restore xUnit1026
+            string name)
+        {
+            Assert.Matches(@"^[a-z]", name);
+        }
+    }
+}

commit d9a3f3e55601bdc475e5469a882a27ddfc1adfbe

Fix broken modifier tests
Broken by commit 8ac523cfd654538a700d8e4d987470746f127dcb.

src/Amadevus.RecordGenerator.Generators/RecordPartialGenerator.cs
@@ -100,11 +100,14 @@ namespace Amadevus.RecordGenerator.Generators
             return Descriptor.Entries.Select(CreateRecordMutator);
             MethodDeclarationSyntax CreateRecordMutator(RecordDescriptor.Entry entry)
             {
+                var valueIdentifier = Identifier(Names.Value);
+
                 var arguments = Descriptor.Entries.Select(x =>
                 {
                     return Argument(
-                        IdentifierName(x.Identifier));
+                        IdentifierName(x == entry ? valueIdentifier : x.Identifier));
                 });
+
                 var mutator =
                     MethodDeclaration(
                         Descriptor.Type,
@@ -112,7 +115,7 @@ namespace Amadevus.RecordGenerator.Generators
                     .AddModifiers(SyntaxKind.PublicKeyword)
                     .WithParameters(
                         Parameter(
-                            Identifier(Names.Value))
+                            valueIdentifier)
                         .WithType(entry.Type))
                     .WithBodyStatements(
                         ReturnStatement(