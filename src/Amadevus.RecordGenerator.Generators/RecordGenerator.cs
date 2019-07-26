246a661304435944d26b84f12d53fbdc4b6603d8
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-07-26 08:55:23 +0200

    Resolve Features during generation

--- src/Amadevus.RecordGenerator.Generators/RecordGenerator.cs
+++ src/Amadevus.RecordGenerator.Generators/RecordGenerator.cs
@@ -11,17 +11,21 @@ namespace Amadevus.RecordGenerator.Generators
 {
     public class RecordGenerator : ICodeGenerator
     {
+        private readonly AttributeData attributeData;
+
         public RecordGenerator(AttributeData attributeData)
         {
-
+            this.attributeData = attributeData;
         }
 
         public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
         {
             var generatedMembers = SyntaxFactory.List<MemberDeclarationSyntax>();
+            var features = GetFeatures(context);
+
             if (context.ProcessingNode is ClassDeclarationSyntax classDeclaration)
             {
-                var descriptor = classDeclaration.ToRecordDescriptor();
+                var descriptor = classDeclaration.ToRecordDescriptor(features);
                 generatedMembers = generatedMembers.AddRange(GenerateRecordPartials(descriptor));
             }
             return Task.FromResult(generatedMembers);
@@ -36,6 +40,28 @@ namespace Amadevus.RecordGenerator.Generators
                 yield return BuilderPartialGenerator.Generate(descriptor, cancellationToken);
                 yield return DeconstructPartialGenerator.Generate(descriptor, cancellationToken);
                 yield break;
+            }
+        }
+
+        private Features GetFeatures(TransformationContext context)
+        {
+            if (attributeData.ConstructorArguments.Length > 0)
+            {
+                return (Features)attributeData.ConstructorArguments[0].Value;
+            }
+            return GetAssemblyDefault() ?? Features.Default;
+
+            Features? GetAssemblyDefault()
+            {
+                foreach (var attribute in context.Compilation.Assembly.GetAttributes())
+                {
+                    if (attribute.AttributeClass.Name == nameof(DefaultRecordFeaturesAttribute)
+                        && attribute.AttributeClass.ToDisplayString() == "Amadevus.RecordGenerator.DefaultRecordFeaturesAttribute")
+                    {
+                        return (Features)attribute.ConstructorArguments[0].Value;
+                    }
+                }
+                return null;
             }
         }
     }
