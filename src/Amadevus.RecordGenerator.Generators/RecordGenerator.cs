04a3efef48878e1c7d753460afc8be7bc3c7e9dc
Atif Aziz <code@raboof.com>
2019-07-31 18:37:31 +0200

    Refactor switch to conditional with local function

--- src/Amadevus.RecordGenerator.Generators/RecordGenerator.cs
+++ src/Amadevus.RecordGenerator.Generators/RecordGenerator.cs
@@ -31,44 +31,43 @@ namespace Amadevus.RecordGenerator.Generators
 
         public Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(TransformationContext context, IProgress<Diagnostic> progress, CancellationToken cancellationToken)
         {
-            switch (context.ProcessingNode)
+            return context.ProcessingNode is ClassDeclarationSyntax cds
+                 ? GenerateAsync(cds)
+                 : EmptyResultTask;
+
+            Task<SyntaxList<MemberDeclarationSyntax>> GenerateAsync(ClassDeclarationSyntax classDeclaration)
             {
-                case ClassDeclarationSyntax classDeclaration:
+                var descriptor = classDeclaration.ToRecordDescriptor();
+                if (descriptor.Entries.IsEmpty)
+                    return EmptyResultTask;
+
+                var generatedMembers = new List<MemberDeclarationSyntax>();
+                var features = GetFeatures();
+                var partialKeyword = Token(SyntaxKind.PartialKeyword);
+
+                var partials =
+                    from g in PartialGenerators
+                    select g.Generate(descriptor, features)
+                    into g
+                    where g != null
+                    select
+                        ClassDeclaration(classDeclaration.Identifier.WithoutTrivia())
+                            .WithTypeParameterList(classDeclaration.TypeParameterList?.WithoutTrivia())
+                            .WithBaseList(g.BaseTypes.IsEmpty ? null : BaseList(SeparatedList(g.BaseTypes)))
+                            .WithModifiers(TokenList(g.Modifiers.Except(new[] {partialKeyword})
+                                .Append(partialKeyword)))
+                            .WithMembers(List(g.Members))
+                            .AddGeneratedCodeAttributeOnMembers();
+
+                foreach (var partial in partials)
                 {
-                    var descriptor = classDeclaration.ToRecordDescriptor();
-                    if (descriptor.Entries.IsEmpty)
-                        return EmptyResultTask;
-
-                    var generatedMembers = new List<MemberDeclarationSyntax>();
-                    var features = GetFeatures();
-                    var partialKeyword = Token(SyntaxKind.PartialKeyword);
-
-                    var partials =
-                        from g in PartialGenerators
-                        select g.Generate(descriptor, features) into g
-                        where g != null
-                        select
-                            ClassDeclaration(classDeclaration.Identifier.WithoutTrivia())
-                                .WithTypeParameterList(classDeclaration.TypeParameterList?.WithoutTrivia())
-                                .WithBaseList(g.BaseTypes.IsEmpty ? null : BaseList(SeparatedList(g.BaseTypes)))
-                                .WithModifiers(TokenList(g.Modifiers.Except(new[] { partialKeyword })
-                                                                    .Append(partialKeyword)))
-                                .WithMembers(List(g.Members))
-                                .AddGeneratedCodeAttributeOnMembers();
-
-                    foreach (var partial in partials)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        generatedMembers.Add(partial);
-                    }
-
-                    return generatedMembers.Count > 0
-                         ? Task.FromResult(List(generatedMembers))
-                         : EmptyResultTask;
+                    cancellationToken.ThrowIfCancellationRequested();
+                    generatedMembers.Add(partial);
                 }
 
-                default:
-                    return EmptyResultTask;
+                return generatedMembers.Count > 0
+                    ? Task.FromResult(List(generatedMembers))
+                    : EmptyResultTask;
             }
         }
 
