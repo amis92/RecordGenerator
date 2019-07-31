cb5849f057617166095d9bb39d2c935628649f61
Jann Flepp <flja@hotmail.ch>
2019-07-31 13:21:57 +0200

    Added feature documentation

--- src/Amadevus.RecordGenerator.Attributes/Features.cs
+++ src/Amadevus.RecordGenerator.Attributes/Features.cs
@@ -42,16 +42,45 @@ namespace Amadevus.RecordGenerator
         /// <c>Deconstruct</c> method:
         /// <see href="https://docs.microsoft.com/pl-pl/dotnet/csharp/deconstruct#deconstructing-user-defined-types"/>
         /// </summary>
-        Deconstruct = 0b_10000,
+        Deconstruct = 0b_1000_0,
 
         /// <summary>
         /// Default feature set.
         /// </summary>
         Default = Constructor | Withers | ToString | Builder | Deconstruct,
 
-        ObjectEquals = 0b_100000,
-        EquatableEquals = 0b_1000000,
-        OperatorEquals = 0b_10000000,
+        /// <summary>
+        /// <see cref="object.Equals(object)"/> and <see cref="object.GetHashCode()"/> overrides. 
+        /// <see cref="object.Equals(object)"/> implements a equality comparison which compares 
+        /// all record entries (properties that are read-only and auto-implemented) 
+        /// of the other <see cref="object"/> with it's own values. 
+        /// <see cref="object.GetHashCode()"/> implements a hash code calculation which calculates
+        /// a hash code using all record entries.
+        /// <see cref="object.GetHashCode()"/>
+        /// </summary>
+        /// <remarks>
+        /// <see cref="object.Equals(object)"/> returns <see langword="true" /> if all values match 
+        /// and returns <see langword="false"/> if <see langword="null" /> is passed, another type is passed or at least one value doesn't mach.
+        /// </remarks>
+        ObjectEquals = 0b_1000_00,
+
+        /// <summary>
+        /// <see cref="IEquatable{T}.Equals(T)"/> implementation that provides a call to the 
+        /// <see cref="object.Equals(object)"/> method in a type safe way. 
+        /// Is dependent on the feature <see cref="ObjectEquals"/>.
+        /// </summary>
+        EquatableEquals = 0b_1000_000,
+
+        /// <summary>
+        /// Overrides a the <see langword="==" /> by using the <see cref="object.Equals(object)"/> method.
+        /// Is dependent on the feature <see cref="ObjectEquals"/>.
+        /// </summary>
+        OperatorEquals = 0b_1000_0000,
+
+        /// <summary>
+        /// Equality feature set which inlcudes 
+        /// <see cref="ObjectEquals"/>, <see cref="EquatableEquals"/> and <see cref="OperatorEquals"/>.
+        /// </summary>
         Equality = ObjectEquals | EquatableEquals | OperatorEquals
     }
 }
