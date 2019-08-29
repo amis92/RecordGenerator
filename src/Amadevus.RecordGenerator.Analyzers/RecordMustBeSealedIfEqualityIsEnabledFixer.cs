f932cd56188008e76ff74b69fd8814b6f2c0f76d
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-08-30 00:18:06 +0200

    Analyzer for sealed-ness of equality records

---
+++ src/Amadevus.RecordGenerator.Analyzers/RecordMustBeSealedIfEqualityIsEnabledFixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Amadevus.RecordGenerator.Analyzers.CodeActions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Amadevus.RecordGenerator.Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+    public sealed class RecordMustBeSealedIfEqualityIsEnabledFixer : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; }
+            = ImmutableArray.Create(Descriptors.X1001_RecordMustBeSealedIfEqualityIsEnabled.Id);
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var typeDeclaration = root.FindNode(context.Span).FirstAncestorOrSelf<TypeDeclarationSyntax>();
+            var document = context.Document;
+            context.RegisterCodeFix(
+                Actions.MakeSealed(document, root, typeDeclaration),
+                context.Diagnostics);
+        }
+    }
+}
--- src/Amadevus.RecordGenerator.Analyzers/Utilities/RecordAttributeExtensions.cs
+++ src/Amadevus.RecordGenerator.Analyzers/Utilities/RecordAttributeExtensions.cs
@@ -1,19 +1,26 @@
 ﻿using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Amadevus.RecordGenerator.Analyzers
 {
     internal static class RecordAttributeExtensions
     {
+        private const string RecordAttributeClassName = "RecordAttribute";
+
         public static bool IsRecordAttributeSyntax(this AttributeSyntax attSyntax)
         {
             return attSyntax.GetUnqualifiedName()?.IsRecordAttributeName() ?? false;
+        }
+        public static bool IsRecordAttribute(this AttributeData attributeData)
+        {
+            return attributeData.AttributeClass.Name == RecordAttributeClassName;
         }
 
         public static bool IsRecordAttributeName(this string text)
         {
             return !string.IsNullOrWhiteSpace(text)
-                && (text == "Record" || text == "RecordAttribute");
+                && (text == "Record" || text == RecordAttributeClassName);
         }
 
         public static string GetUnqualifiedName(this AttributeSyntax attSyntax)
